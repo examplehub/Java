9fb31d667f12a732ae32e5c7c98670ca5fd7ccb9
shellhub <shellhub.me@gmail.com>
2020-09-27 19:06:33 +0800

    if

---
+++ src/main/java/com/examplehub/basics/If.java
@@ -0,0 +1,17 @@
+package com.examplehub.basics;
+
+public class If {
+    public static void main(String[] args) {
+        int a = 3; /* change 3 to what you want */
+        int b = 4; /* change 4 to what you want */
+        if (a == b) {
+            System.out.println("a == b");
+        }
+        if (a > b) {
+            System.out.println("a > b");
+        }
+        if (a < b) {
+            System.out.println("a < b");
+        }
+    }
+}
