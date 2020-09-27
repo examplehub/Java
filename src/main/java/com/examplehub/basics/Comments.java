a7fb34d98c8524ed1195c4316a9ff79da1a1d966
shellhub <shellhub.me@gmail.com>
2020-09-27 18:58:25 +0800

    Comments

---
+++ src/main/java/com/examplehub/basics/Comments.java
@@ -0,0 +1,20 @@
+package com.examplehub.basics;
+
+public class Comments {
+
+    /**
+     * Driver Code
+     *
+     * @param args the arguments passed to main function
+     */
+    public static void main(String[] args) {
+        System.out.println("This is a single comment"); // Single-line comments start with two forward slashes (//).
+        System.out.println("This is a another single comment"); /* single or multi line */
+
+        /*
+         *This is a  multi-line comments.
+         * This will print ExampleHub.
+         */
+        System.out.println("ExampleHub");
+    }
+}
