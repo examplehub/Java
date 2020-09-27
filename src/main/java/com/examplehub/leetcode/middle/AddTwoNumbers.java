4374b03b4f567a9e248c08918c4cd37b00b9c97b
shellhub <shellhub.me@gmail.com>
2020-09-28 01:30:20 +0800

    added AddTwoNumbers solution2

--- src/main/java/com/examplehub/leetcode/middle/AddTwoNumbers.java
+++ src/main/java/com/examplehub/leetcode/middle/AddTwoNumbers.java
@@ -5,6 +5,7 @@ package com.examplehub.leetcode.middle;
  */
 public class AddTwoNumbers {
 
+    /* solution1 can't process large input */
     public static ListNode solution1(ListNode l1, ListNode l2) {
         StringBuilder builder1 = new StringBuilder();
         while (l1 != null) {
@@ -35,6 +36,31 @@ public class AddTwoNumbers {
         } while (sum != 0);
         return head;
     }
+
+    public static ListNode solution2(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode p = l1, q = l2, curr = dummyHead;
+        int carry = 0;
+        while (p != null || q != null) {
+            int x = p != null ? p.val : 0;
+            int y = q != null ? q.val : 0;
+            int sum = carry + x + y;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (p != null) {
+                p = p.next;
+            }
+            if (q != null) {
+                q = q.next;
+            }
+        }
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+        return dummyHead.next;
+    }
+
 }
 
 class ListNode {
--- src/test/java/com/examplehub/leetcode/middle/AddTwoNumbersTest.java
+++ src/test/java/com/examplehub/leetcode/middle/AddTwoNumbersTest.java
@@ -44,4 +44,44 @@ class AddTwoNumbersTest {
         builder.append("NULL");
         assertEquals("0->NULL", builder.toString());
     }
+
+    @Test
+    void solution2() {
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
+        l1.next.next.next = null;
+
+        ListNode l2 = new ListNode(5);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(4);
+        l2.next.next.next = null;
+
+        assertEquals(l1.next.next.val, 3);
+        assertEquals(l2.next.next.val, 4);
+
+
+        ListNode result = AddTwoNumbers.solution2(l1, l2);
+
+        StringBuilder builder = new StringBuilder();
+        while (result != null) {
+            builder.append(result.val).append("->");
+            result = result.next;
+        }
+
+        builder.append("NULL");
+        assertEquals("7->0->8->NULL", builder.toString());
+
+        l1 = new ListNode(0);
+        l2 = new ListNode(0);
+        result = AddTwoNumbers.solution2(l1, l2);
+        builder.delete(0, builder.length());
+        while (result != null) {
+            builder.append(result.val).append("->");
+            result = result.next;
+        }
+        builder.append("NULL");
+        assertEquals("0->NULL", builder.toString());
+
+    }
 }
