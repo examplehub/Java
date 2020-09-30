6b769731b175c2d63ffd637ad637a6db8e5ba8c5
shellhub <shellhub.me@gmail.com>
2020-10-01 00:39:26 +0800

    Median of Two Sorted Arrays

---
+++ src/main/java/com/examplehub/leetcode/hard/MedianOfTwoSortedArrays.java
@@ -0,0 +1,20 @@
+package com.examplehub.leetcode.hard;
+
+import com.examplehub.maths.Median;
+
+public class MedianOfTwoSortedArrays {
+
+    /**
+     * Return the median of two sorted array.
+     *
+     * @param numbers1 the first array.
+     * @param numbers2 the second array.
+     * @return median of two sorted array.
+     */
+    public static double solution1(double[] numbers1, double[] numbers2) {
+        double[] numbers = new double[numbers1.length + numbers2.length];
+        System.arraycopy(numbers1, 0, numbers, 0, numbers1.length);
+        System.arraycopy(numbers2, 0, numbers, numbers1.length, numbers2.length);
+        return Median.median(numbers);
+    }
+}
---
+++ src/test/java/com/examplehub/leetcode/hard/MedianOfTwoSortedArraysTest.java
@@ -0,0 +1,16 @@
+package com.examplehub.leetcode.hard;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MedianOfTwoSortedArraysTest {
+
+    @Test
+    void testSolution1() {
+        assertEquals(2.0, MedianOfTwoSortedArrays.solution1(new double[]{1, 3}, new double[]{2}));
+        assertEquals(2.5, MedianOfTwoSortedArrays.solution1(new double[]{1, 2}, new double[]{3, 4}));
+        assertEquals(0.0, MedianOfTwoSortedArrays.solution1(new double[]{0,0}, new double[]{0, 0}));
+        assertEquals(1.0, MedianOfTwoSortedArrays.solution1(new double[]{}, new double[]{1}));
+    }
+}
