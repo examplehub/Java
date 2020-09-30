1ef157b4851ffad6c9d41ee75499a7ab2d2776c7
shellhub <shellhub.me@gmail.com>
2020-09-30 17:35:22 +0800

    add count digits recursion

---
+++ src/main/java/com/examplehub/maths/CountDigitsRecursion.java
@@ -0,0 +1,15 @@
+package com.examplehub.maths;
+
+public class CountDigitsRecursion {
+
+    /**
+     * Count the number of digits of a number using recursion.
+     *
+     * @param number the number to counted.
+     * @return the number of digits.
+     */
+    public static int countDigits(int number) {
+        number = Math.abs(number);
+        return number < 10 ? 1 : 1 + countDigits(number / 10);
+    }
+}
