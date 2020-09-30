94b4a326ace70928ec3d3e72250cd7680c76e9c2
shellhub <shellhub.me@gmail.com>
2020-09-30 20:23:05 +0800

    add cube number

--- src/main/java/com/examplehub/maths/CubeNumber.java
+++ src/main/java/com/examplehub/maths/CubeNumber.java
@@ -1,2 +1,23 @@
-package com.examplehub.maths;public class CubeNumber {
+package com.examplehub.maths;
+
+/**
+ * https://en.wikipedia.org/wiki/Cube_(algebra)
+ */
+public class CubeNumber {
+
+    /**
+     * Test if a number is cube number or not.
+     *
+     * @param number the number to checked.
+     * @return {@code true} if given {@code number} is cube number, otherwise {@code false}.
+     */
+    public static boolean isCubeNumber(int number) {
+        number = Math.abs(number);
+        for (int i = 0; i <= number; ++i) {
+            if (i * i * i == number) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
