b68f907c8bdff143c9550c43338208f53ddc5439
shellhub <shellhub.me@gmail.com>
2020-09-30 20:52:37 +0800

    add average

---
+++ src/main/java/com/examplehub/maths/Average.java
@@ -0,0 +1,21 @@
+package com.examplehub.maths;
+
+/**
+ * https://en.wikipedia.org/wiki/Average
+ */
+public class Average {
+
+    /**
+     * Calculate average value of numbers.
+     *
+     * @param numbers the numbers to be calculated.
+     * @return average value of numbers.
+     */
+    public static double average(double[] numbers) {
+        double sum = 0;
+        for (double number : numbers) {
+            sum += number;
+        }
+        return sum / numbers.length;
+    }
+}
---
+++ src/test/java/com/examplehub/maths/AverageTest.java
@@ -0,0 +1,15 @@
+package com.examplehub.maths;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AverageTest {
+
+    @Test
+    void testAverage() {
+        assertEquals(12.0, Average.average(new double[]{3, 6, 9, 12, 15, 18, 21}));
+        assertEquals(5.5, Average.average(new double[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}));
+        assertEquals(3.3, Average.average(new double[]{1.1, 2.2, 3.3, 4.4, 5.5}));
+    }
+}
