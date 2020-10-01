4abb7db85cee84913a6024bf51c8345c2f5effed
shellhub <shellhub.me@gmail.com>
2020-10-01 14:27:48 +0800

    add multiply

---
+++ src/main/java/com/examplehub/maths/Multiply.java
@@ -0,0 +1,26 @@
+package com.examplehub.maths;
+
+public class Multiply {
+
+    /**
+     * Multiply two integer numbers.
+     *
+     * @param a the first integer number.
+     * @param b the second integer number.
+     * @return the multiply of two integer numbers.
+     */
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    /**
+     * Multiply two double numbers.
+     *
+     * @param a the first double number.
+     * @param b the second double number.
+     * @return the multiply of two double numbers.
+     */
+    public static double multiply(double a, double b) {
+        return a * b;
+    }
+}
