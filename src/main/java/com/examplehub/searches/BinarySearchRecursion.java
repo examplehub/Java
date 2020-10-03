0f7102b8719623723864a32c07b75796de92dc9e
shellhub <shellhub.me@gmail.com>
2020-10-03 20:36:16 +0800

    add binary search

---
+++ src/main/java/com/examplehub/searches/BinarySearchRecursion.java
@@ -0,0 +1,62 @@
+package com.examplehub.searches;
+
+public class BinarySearchRecursion implements Search {
+
+    @Override
+    public int search(int[] numbers, int key) {
+        return search(numbers, 0, numbers.length - 1, key);
+    }
+
+    /**
+     * Binary search algorithm using recursion.
+     *
+     * @param numbers the numbers to be searched.
+     * @param left    the left index of left sub array.
+     * @param right   the right index of right sub array.
+     * @param key     the key to be searched.
+     * @return index of {@code key} value if found, otherwise -1.
+     */
+    public int search(int[] numbers, int left, int right, int key) {
+        if (left > right) {
+            return -1;
+        }
+        int mid = (left + right) >> 1;
+        if (key == numbers[mid]) {
+            return mid;
+        } else if (key >= numbers[mid]) {
+            return search(numbers, mid + 1, right, key); /* search at right sub array */
+        } else {
+            return search(numbers, left, mid - 1, key); /* search at left sub array */
+        }
+    }
+
+
+    @Override
+    public <T extends Comparable<T>> int search(T[] array, T key) {
+        return search(array, 0, array.length - 1, key);
+    }
+
+    /**
+     * Generic binary search algorithm using recursion.
+     *
+     * @param array the array to be searched.
+     * @param left  the left index of left sub array.
+     * @param right the right index of right sub array.
+     * @param key   the key object to be searched.
+     * @param <T>   the class of the objects in the array.
+     * @return index of {@code key} if found, otherwise -1.
+     */
+    public <T extends Comparable<T>> int search(T[] array, int left, int right, T key) {
+        if (left > right) {
+            return -1;
+        }
+        int mid = (left + right) >> 1;
+        if (key.compareTo(array[mid]) == 0) {
+            return mid;
+        } else if (key.compareTo(array[mid]) > 0) {
+            return search(array, mid + 1, right, key); /* search at right sub array */
+        } else {
+            return search(array, left, mid - 1, key); /* search at left sub array */
+        }
+    }
+}
