4dabd5331924f7fdde99a96d7771880285b952b1
shellhub <shellhub.me@gmail.com>
2020-10-05 11:37:37 +0800

    add power

---
+++ src/test/java/com/examplehub/maths/PowerTest.java
@@ -0,0 +1,19 @@
+package com.examplehub.maths;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PowerTest {
+
+    @Test
+    void testPow() {
+        assertEquals(1, Power.pow(0, 0));
+        assertEquals(0, Power.pow(0, 10));
+        assertEquals(1, Power.pow(1, 0));
+        assertEquals(2, Power.pow(2, 1));
+        assertEquals(8, Power.pow(2, 3));
+        assertEquals(1024, Power.pow(2, 10));
+        assertEquals(625, Power.pow(25, 2));
+    }
+}
