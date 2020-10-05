6cc7801c05262a756e87e334e2022e2ed758314e
shellhub <shellhub.me@gmail.com>
2020-10-05 16:23:34 +0800

    fixed LGTM

---
+++ src/main/java/com/examplehub/projecteuler/Problem10.java
@@ -0,0 +1,14 @@
+package com.examplehub.projecteuler;
+
+/**
+ * The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+ * <p>
+ * Find the sum of all the primes below two million.
+ * <p>
+ * link: https://projecteuler.net/problem=10
+ */
+public class Problem10 {
+    public static long solution1(int n) {
+        long sum = 0;
+    }
+}
