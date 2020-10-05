d4f0eed4dcf22627de6eaccb8b7b33cfee937ce9
shellhub <shellhub.me@gmail.com>
2020-10-05 16:29:28 +0800

    add Problem10

--- src/main/java/com/examplehub/projecteuler/Problem06.java
+++ src/main/java/com/examplehub/projecteuler/Problem06.java
@@ -16,7 +16,7 @@ public class Problem06 {
     public static long solution1(int n) {
         long sumOfSquares = 0;
         for (int i = 1; i <= n; ++i) {
-            sumOfSquares += Math.pow(i, 2);
+            sumOfSquares  = (long) (sumOfSquares + Math.pow(i, 2));
         }
 
         long squareOfSum = (long) Math.pow(SumToN.sum(n), 2);
--- src/main/java/com/examplehub/projecteuler/Problem10.java
+++ src/main/java/com/examplehub/projecteuler/Problem10.java
@@ -1,5 +1,7 @@
 package com.examplehub.projecteuler;
 
+import com.examplehub.maths.PrimeCheck;
+
 /**
  * The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
  * <p>
@@ -10,5 +12,11 @@ package com.examplehub.projecteuler;
 public class Problem10 {
     public static long solution1(int n) {
         long sum = 0;
+        for (int i = 2; i <= n; i++) {
+            if (PrimeCheck.isPrime(i)) {
+                sum += i;
+            }
+        }
+        return sum;
     }
 }
