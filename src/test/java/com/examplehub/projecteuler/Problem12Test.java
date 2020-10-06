de8a5a51a9a8166f3a13f0574c90d22de4641328
shellhub <shellhub.me@gmail.com>
2020-10-06 18:43:36 +0800

    add Problem12

---
+++ src/test/java/com/examplehub/projecteuler/Problem12Test.java
@@ -0,0 +1,13 @@
+package com.examplehub.projecteuler;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Problem12Test {
+    @Test
+    void test() {
+        assertEquals(28, Problem12.solution1(5));
+//        assertEquals(76576500, Problem12.solution1(500));
+    }
+}
