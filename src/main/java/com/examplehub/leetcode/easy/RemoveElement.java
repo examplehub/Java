bd4f1f0ea00c3ab5d101cc1f21f6942bb0668b14
shellhub <shellhub.me@gmail.com>
2020-10-06 13:40:15 +0800

    RemoveElement

--- src/main/java/com/examplehub/leetcode/easy/RemoveElement.java
+++ src/main/java/com/examplehub/leetcode/easy/RemoveElement.java
@@ -15,4 +15,15 @@ public class RemoveElement {
         }
         return length;
     }
+
+    public static int solution2(int[] nums, int val) {
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
 }
--- src/test/java/com/examplehub/leetcode/easy/RemoveElementTest.java
+++ src/test/java/com/examplehub/leetcode/easy/RemoveElementTest.java
@@ -7,13 +7,27 @@ import static org.junit.jupiter.api.Assertions.*;
 class RemoveElementTest {
 
     @Test
-    void test() {
+    void testSolution1() {
         int[] ints = new int[]{3, 2, 2, 3};
         assertEquals(2, RemoveElement.solution1(ints, 3));
         assertTrue(ints[0] == 2 && ints[1] == 2);
 
         ints = new int[]{0, 1, 2, 2, 3, 0, 4, 2};
         assertEquals(5, RemoveElement.solution1(ints, 2));
+        int[] newInts = {0, 1, 3, 0, 4};
+        for (int i = 0; i < newInts.length; i++) {
+            assertEquals(ints[i], newInts[i]);
+        }
+    }
+
+    @Test
+    void testSolution2() {
+        int[] ints = new int[]{3, 2, 2, 3};
+        assertEquals(2, RemoveElement.solution2(ints, 3));
+        assertTrue(ints[0] == 2 && ints[1] == 2);
+
+        ints = new int[]{0, 1, 2, 2, 3, 0, 4, 2};
+        assertEquals(5, RemoveElement.solution2(ints, 2));
         int[] newInts = {0, 1, 3, 0, 4};
         for (int i = 0; i < newInts.length; i++) {
             assertEquals(ints[i], newInts[i]);
