9b5c405d4396d7da442738f7147c853c29e33936
shellhub <shellhub.me@gmail.com>
2020-10-07 11:42:36 +0800

    update Reverse Number

--- src/main/java/com/examplehub/maths/ReverseNumber.java
+++ src/main/java/com/examplehub/maths/ReverseNumber.java
@@ -16,4 +16,19 @@ public class ReverseNumber {
         }
         return reversedNumber;
     }
+
+    /**
+     * Reverse a number.
+     *
+     * @param number the number to be reversed.
+     * @return the reversed number of given number.
+     */
+    public static int reverse2(int number) {
+        boolean isNegative = number < 0;
+        number = Math.abs(number);
+
+        StringBuilder builder = new StringBuilder(number + "");
+        builder = builder.reverse();
+        return isNegative ? -Integer.parseInt(builder.toString()) : Integer.parseInt(builder.toString());
+    }
 }
--- src/test/java/com/examplehub/maths/ReverseNumberTest.java
+++ src/test/java/com/examplehub/maths/ReverseNumberTest.java
@@ -14,4 +14,14 @@ class ReverseNumberTest {
         assertEquals(-21, ReverseNumber.reverse(-12));
         assertEquals(-54321, ReverseNumber.reverse(-12345));
     }
+
+    @Test
+    void testReverseNumber1() {
+        assertEquals(21, ReverseNumber.reverse2(12));
+        assertEquals(12345, ReverseNumber.reverse2(54321));
+        assertEquals(54321, ReverseNumber.reverse2(12345));
+
+        assertEquals(-21, ReverseNumber.reverse2(-12));
+        assertEquals(-54321, ReverseNumber.reverse2(-12345));
+    }
 }
