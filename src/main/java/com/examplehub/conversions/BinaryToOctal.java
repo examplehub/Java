0a3433cf73bc3d67798b35f7f83025f97a75f2bd
shellhub <shellhub.me@gmail.com>
2020-10-08 19:48:07 +0800

    add compatible

--- src/main/java/com/examplehub/conversions/BinaryToOctal.java
+++ src/main/java/com/examplehub/conversions/BinaryToOctal.java
@@ -9,10 +9,12 @@ public class BinaryToOctal {
      * @return octal string of a binary string.
      */
     public static String toOctal(String binaryString) {
-        StringBuilder builder = new StringBuilder();
+        StringBuilder builder = new StringBuilder(binaryString);
         if (binaryString.length() % 3 != 0) {
-            builder.append("0".repeat(3 - binaryString.length() % 3));
-            binaryString = builder.append(binaryString).toString();
+            // just wok on jdk 11+
+            //builder.append("0".repeat(3 - binaryString.length() % 3));
+            binaryString = (binaryString.length() % 3 == 1 ?
+                    builder.insert(0, "00") : builder.insert(0, "0")).toString();
         }
         StringBuilder result = new StringBuilder();
         for (int i = 0; i < binaryString.length() / 3; i++) {
