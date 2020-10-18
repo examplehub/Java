7950f540b139e49bcd5387c14d70e99694d77083
shellhub <shellhub.me@gmail.com>
2020-10-18 11:42:49 +0800

    add ceil

---
+++ src/main/java/com/examplehub/maths/Ceil.java
@@ -0,0 +1,7 @@
+package com.examplehub.maths;
+
+public class Ceil {
+    public static int ceil(double number) {
+        return number - (int) number <= 0 ? (int) number : (int) number + 1;
+    }
+}
