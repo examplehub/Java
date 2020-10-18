a37d4ec485c03fb87edc1a5b14b27c54d9454321
shellhub <shellhub.me@gmail.com>
2020-10-18 12:14:25 +0800

    add problem 30

---
+++ src/main/java/com/examplehub/projecteuler/Problem30.java
@@ -0,0 +1,23 @@
+package com.examplehub.projecteuler;
+
+/**
+ * https://projecteuler.net/problem=30
+ */
+public class Problem30 {
+
+    public static int solution1() {
+        int sum = 0;
+        for (int i = 2; i <= 1000000; i++) {
+            int number = i;
+            int tempSum = 0;
+            while (number != 0) {
+                tempSum = (int) (tempSum + Math.pow(number % 10, 5));
+                number /= 10;
+            }
+            if (tempSum == i) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+}
---
+++ src/test/java/com/examplehub/projecteuler/Problem30Test.java
@@ -0,0 +1,12 @@
+package com.examplehub.projecteuler;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Problem30Test {
+    @Test
+    void testSolution1() {
+        assertEquals(443839, Problem30.solution1());
+    }
+}
