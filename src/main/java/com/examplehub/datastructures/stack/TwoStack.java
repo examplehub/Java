b47bc75092389b7154f2d992a5dd2457e2e22f92
shellhub <shellhub.me@gmail.com>
2020-10-22 09:41:18 +0800

    add two stack

---
+++ src/main/java/com/examplehub/datastructures/stack/TwoStack.java
@@ -0,0 +1,135 @@
+package com.examplehub.datastructures.stack;
+
+import java.util.EmptyStackException;
+
+public class TwoStack<E> {
+
+    /**
+     * The stack to store elements.
+     */
+    private final E[] stack;
+
+    /**
+     * The default capacity.
+     */
+    private static final int DEFAULT_CAPACITY = 10;
+
+    /**
+     * The capacity of this stack.
+     */
+    private final int capacity;
+
+    /**
+     * The first top of first stack.
+     */
+    private int top1;
+
+    /**
+     * The second top of second stack.
+     */
+    private int top2;
+
+    public TwoStack() {
+        this(DEFAULT_CAPACITY);
+    }
+
+    public TwoStack(int capacity) {
+        @SuppressWarnings("unchecked")
+        E[] tempStack = (E[]) new Object[capacity];
+        stack = tempStack;
+        this.capacity = capacity;
+        this.top1 = -1;
+        this.top2 = capacity;
+    }
+
+    /**
+     * Push item to first stack.
+     *
+     * @param item the item to be added to first stack.
+     * @return {@code true} if added successfully
+     * @throws EmptyStackException if the stack is full.
+     */
+    public boolean push1(E item) {
+        if (full()) {
+            throw new EmptyStackException();
+        }
+        stack[++top1] = item;
+        return true;
+    }
+
+    /**
+     * Push item to second stack.
+     *
+     * @param item the item to be added to second stack.
+     * @return {@code true} if added successfully
+     * @throws EmptyStackException if the stack is full.
+     */
+    public boolean push2(E item) {
+        if (full()) {
+            throw new EmptyStackException();
+        }
+        stack[--top2] = item;
+        return true;
+    }
+
+    /**
+     * Remove item from the top of first stack.
+     *
+     * @return item at the top of first stack.
+     * @throws EmptyStackException if first stack is empty.
+     */
+    public E pop1() {
+        if (top1 == -1) {
+            throw new EmptyStackException();
+        }
+        return stack[top1--];
+    }
+
+    /**
+     * Remove item from the top of second stack.
+     *
+     * @return item at the top of second stack.
+     * @throws EmptyStackException if second stack is empty.
+     */
+    public E pop2() {
+        if (top2 == capacity) {
+            throw new EmptyStackException();
+        }
+        return stack[top2++];
+    }
+
+    /**
+     * Return the number of elements in this stack.
+     *
+     * @return size of this stack.
+     */
+    public int size() {
+        return (top1 + 1) + (capacity - top2);
+    }
+
+    /**
+     * Clear all elements in the stack.
+     */
+    public void clear() {
+        top1 = -1;
+        top2 = capacity;
+    }
+
+    /**
+     * Returns {@code true} if this stack contains no elements.
+     *
+     * @return {@code true} if this stack contains no elements, otherwise {@code false}.
+     */
+    public boolean empty() {
+        return size() == 0;
+    }
+
+    /**
+     * Test if this stack is full.
+     *
+     * @return {@code true} if this stack is full, otherwise {@code false}.
+     */
+    public boolean full() {
+        return size() == capacity;
+    }
+}
---
+++ src/test/java/com/examplehub/datastructures/stack/TwoStackTest.java
@@ -0,0 +1,82 @@
+package com.examplehub.datastructures.stack;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.EmptyStackException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TwoStackTest {
+    @Test
+    void testTwoStack() {
+        TwoStack<Integer> stack = new TwoStack<>(7);
+
+        assertTrue(stack.empty());
+        assertEquals(0, stack.size());
+
+        try {
+            stack.pop1();
+            fail(); /* this should will not happen */
+        } catch (EmptyStackException e) {
+            assertTrue(true); /* this should will happen */
+        }
+
+        try {
+            stack.pop2();
+            fail(); /* this should will not happen */
+        } catch (EmptyStackException e) {
+            assertTrue(true); /* this should will happen */
+        }
+
+        for (int i = 1; i <= 7; i++) {
+            if (i % 2 == 0) {
+                stack.push1(i);
+            } else {
+                stack.push2(i);
+            }
+        }
+
+        assertTrue(stack.full());
+        assertEquals(7, stack.size());
+
+        try {
+            stack.push1(666);
+            fail(); /* this should will not happen */
+        } catch (EmptyStackException e) {
+            assertTrue(true); /* this should will happen */
+        }
+
+        try {
+            stack.push2(666);
+            fail(); /* this should will not happen */
+        } catch (EmptyStackException e) {
+            assertTrue(true); /* this should will happen */
+        }
+
+        assertEquals(6, stack.pop1());
+        assertEquals(4, stack.pop1());
+        assertEquals(2, stack.pop1());
+
+        assertEquals(7, stack.pop2());
+        assertEquals(5, stack.pop2());
+        assertEquals(3, stack.pop2());
+        assertEquals(1, stack.pop2());
+
+        assertEquals(0, stack.size());
+        assertTrue(stack.empty());
+    }
+
+    @Test
+    void testClear() {
+        TwoStack<String> stack = new TwoStack<>(10);
+        for (int i = 1; i <= 10; i++) {
+            if (i % 2 == 0) {
+                stack.push1(i + "");
+            } else {
+                stack.push2(i + "");
+            }
+        }
+        stack.clear();
+        assertTrue(stack.empty());
+    }
+}
