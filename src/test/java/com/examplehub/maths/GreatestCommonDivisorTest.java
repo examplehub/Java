4832c6f25960b8efceeb67f1842a3c80becf6aef
shellhub <shellhub.me@gmail.com>
2020-10-22 10:27:13 +0800

    add gcd

---
+++ src/test/java/com/examplehub/maths/GreatestCommonDivisorTest.java
@@ -0,0 +1,16 @@
+package com.examplehub.maths;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GreatestCommonDivisorTest {
+    @Test
+    void testGCD() {
+        int[][] testNumbers = {{8, 12, 4}, {54, 24, 6}, {3, 4, 1}, {3, 0, 3}};
+        for (int[] testNumber : testNumbers) {
+            assertEquals(testNumber[2], GreatestCommonDivisor.gcdEasy(testNumber[0], testNumber[1]));
+            assertEquals(testNumber[2], GreatestCommonDivisor.gcd(testNumber[0], testNumber[1]));
+        }
+    }
+}
