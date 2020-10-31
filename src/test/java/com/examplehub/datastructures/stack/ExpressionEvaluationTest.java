7a604ad5fa4af9baeff300cff12b29493dd92223
shellhub <shellhub.me@gmail.com>
2020-10-31 11:42:33 +0800

    fixed typo

--- src/test/java/com/examplehub/datastructures/stack/ExpressionEvaluationTest.java
+++ src/test/java/com/examplehub/datastructures/stack/ExpressionEvaluationTest.java
@@ -8,10 +8,10 @@ class ExpressionEvaluationTest {
 
   @Test
   void evaluate() throws Exception {
-    assertEquals(5, 2 + 3);
-    assertEquals(-1, 2 - 3);
-    assertEquals(0, 2 / 3);
-    assertEquals(6, 2 * 3);
+    assertEquals(5, ExpressionEvaluation.evaluate("2 + 3"));
+    assertEquals(-1, ExpressionEvaluation.evaluate("2 - 3"));
+    assertEquals(0, ExpressionEvaluation.evaluate("2 / 3"));
+    assertEquals(6, ExpressionEvaluation.evaluate("2 * 3"));
     assertEquals(80, ExpressionEvaluation.evaluate("1+(2*3^4)/2-2"));
     assertEquals(80, ExpressionEvaluation.evaluate("1 + (2 * 3 ^ 4) / 2 - 2"));
   }
