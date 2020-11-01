6b112c755c2509c81cede4229a1b170972e14720
Du Yuanchao <shellhub.me@gmail.com>
2020-11-01 14:09:47 +0800

    BinaryTree level order traversal (#46)
    * fixed typo
    
    * next greater element
    
    * shutdown hook
    
    * final
    
    * init multi variables
    
    * string on switch
    
    * char to int
    
    * Formatted with Google Java Formatter
    
    * update README
    
    * binary tree level orderl traversal
    
    Co-authored-by: github-actions <${GITHUB_ACTOR}@users.noreply.github.com>

--- src/test/java/com/examplehub/datastructures/binarytree/BasicBinaryTreeTest.java
+++ src/test/java/com/examplehub/datastructures/binarytree/BasicBinaryTreeTest.java
@@ -12,5 +12,6 @@ class BasicBinaryTreeTest {
     assertEquals("1->2->4->5->3", BasicBinaryTree.getBasicBinaryTree().getPreOrder());
     assertEquals("4->2->5->1->3", BasicBinaryTree.getBasicBinaryTree().getInorder());
     assertEquals("4->5->2->3->1", BasicBinaryTree.getBasicBinaryTree().getPostOrder());
+    assertEquals("1->2->3->4->5", BasicBinaryTree.getBasicBinaryTree().getLevelOrder());
   }
 }
