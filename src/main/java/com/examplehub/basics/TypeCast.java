1d3310ffe525a7102ec48b4fd2d7f2392fb49347
Du Yuanchao <shellhub.me@gmail.com>
2020-11-11 22:41:24 +0800

    Typecast (#54)
    * add variable
    
    * data types
    
    * typecast
    
    * Formatted with Google Java Formatter
    
    Co-authored-by: github-actions <${GITHUB_ACTOR}@users.noreply.github.com>

---
+++ src/main/java/com/examplehub/basics/TypeCast.java
@@ -0,0 +1,32 @@
+package com.examplehub.basics;
+
+public class TypeCast {
+  public static void main(String[] args) {
+
+    int number = 'a';
+    /*
+     * 97
+     */
+    System.out.println(number);
+
+    int digit = '8' - '0';
+    /*
+     * 8
+     */
+    System.out.println(digit);
+
+    double price = 3;
+
+    /*
+     * 8
+     */
+    int sum = (int) (3.5 + 4.6);
+    System.out.println(sum);
+
+    char letter = 65;
+    /*
+     * A
+     */
+    System.out.println(letter);
+  }
+}
