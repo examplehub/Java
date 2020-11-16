12e7def204539dc809eb88b9dda988a3d46bf067
Du Yuanchao <shellhub.me@gmail.com>
2020-11-16 20:57:22 +0800

    development (#56)
    * add shell sort
    
    * Formatted with Google Java Formatter
    
    * Frog Jump
    
    * Formatted with Google Java Formatter
    
    * String example
    
    * math example
    
    * add operator
    
    * Formatted with Google Java Formatter
    
    Co-authored-by: github-actions <${GITHUB_ACTOR}@users.noreply.github.com>

---
+++ src/test/java/com/examplehub/maths/FrogJumpTest.java
@@ -0,0 +1,17 @@
+package com.examplehub.maths;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FrogJumpTest {
+
+  @Test
+  void test() {
+    assertEquals(1, FrogJump.canCross(0, 0));
+    assertEquals(2, FrogJump.canCross(1, 0));
+    assertEquals(3, FrogJump.canCross(2, 0));
+    assertEquals(4, FrogJump.canCross(1, 1));
+    assertEquals(6, FrogJump.canCross(2, 1));
+  }
+}
