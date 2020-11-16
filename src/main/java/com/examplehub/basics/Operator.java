0b9acc8423afb11af08cc9bc32b6046cb4315702
Du Yuanchao <shellhub.me@gmail.com>
2020-11-16 09:26:35 +0800

    Add operator (#55)
    * add variable
    
    * data types
    
    * typecast
    
    * operator
    
    * Formatted with Google Java Formatter
    
    Co-authored-by: github-actions <${GITHUB_ACTOR}@users.noreply.github.com>

---
+++ src/main/java/com/examplehub/basics/Operator.java
@@ -0,0 +1,37 @@
+package com.examplehub.basics;
+
+public class Operator {
+  public static void main(String[] args) {
+    int a = 3;
+    int b = 4;
+    System.out.println("a + b = " + (a + b)); /* a + b = 7 */
+    System.out.println("a - b = " + (a - b)); /* a - b = -1 */
+    System.out.println("a * b = " + (a * b)); /* a * b = 12 */
+    System.out.println("a / b = " + (a / b)); /* a / b = 0 */
+    System.out.println("3 / 4.0 = " + 3 / 4.0); /* 3 / 4.0 = 0.75 */
+    System.out.println("a % b = " + (a % b)); /* a % b = 3 */
+
+    int i = 1;
+    i++;
+    System.out.println("i = " + i); /* i = 2 */
+
+    i--;
+    System.out.println("i = " + i); /* i = 1 */
+
+    int j = 5;
+    System.out.println("j++ = " + j++); /* j++ = 5 */
+    System.out.println("j = " + j); /* j = 6 */
+
+    int k = 10;
+    System.out.println("++k = " + ++k); /* ++k = 11 */
+    System.out.println("k = " + k); /* k = 11 */
+
+    System.out.println("3&4 = " + (3 & 4));
+    System.out.println("3|4 = " + (3 | 4));
+    System.out.println("~4 = " + ~4);
+    System.out.println("3^4 = " + (3 ^ 4));
+    System.out.println("4<<1 = " + (4 << 1));
+    System.out.println("4>>1=" + (4 >> 1));
+    System.out.println("4>>>1=" + (4 >>> 1));
+  }
+}
