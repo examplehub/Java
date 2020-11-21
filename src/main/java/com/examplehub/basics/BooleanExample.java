bb668537bd9fb8b493ff3da760d85659697f9225
shellhub <shellhub.me@gmail.com>
2020-11-21 19:38:20 +0800

    add boolean

---
+++ src/main/java/com/examplehub/basics/BooleanExample.java
@@ -0,0 +1,14 @@
+package com.examplehub.basics;
+
+public class BooleanExample {
+  public static void main(String[] args) {
+    System.out.println(true); // true
+    System.out.println(false); // false
+    System.out.println(3 == 4); // false
+    System.out.println(3 == 3); // true
+
+    int age = 18;
+    System.out.println(age >= 10 && age <= 17); // false
+    System.out.println(age >= 10 && age <= 19); // true
+  }
+}
