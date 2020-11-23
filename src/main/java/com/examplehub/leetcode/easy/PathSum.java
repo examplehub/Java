f86cc2ba76d654f5a9e57ffc6a313d9ad45d9454
shellhub <shellhub.me@gmail.com>
2020-11-23 14:13:19 +0800

    add link

--- src/main/java/com/examplehub/leetcode/easy/PathSum.java
+++ src/main/java/com/examplehub/leetcode/easy/PathSum.java
@@ -2,6 +2,9 @@ package com.examplehub.leetcode.easy;
 
 import com.examplehub.datastructures.binarytree.Node;
 
+/**
+ * https://leetcode.com/problems/path-sum/
+ */
 public class PathSum {
   public static boolean solution1(Node<Integer> node, int sum) {
     if (node == null) {
