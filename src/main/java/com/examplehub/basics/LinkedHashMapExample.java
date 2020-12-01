2d8291d15b769df2a58a54ba79251180315baa23
shellhub <shellhub.me@gmail.com>
2020-12-01 11:11:19 +0800

    add LinkedHashMap

---
+++ src/main/java/com/examplehub/basics/LinkedHashMapExample.java
@@ -0,0 +1,24 @@
+package com.examplehub.basics;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class LinkedHashMapExample {
+  public static void main(String[] args) {
+    LinkedHashMap<Integer, String> map = new LinkedHashMap<>();
+    map.put(1, "Java");
+    map.put(2, "C");
+    map.put(3, "Python");
+    System.out.println(map); /* {1=Java, 2=C, 3=Python} */
+
+      /*
+     * key = 1, value = Java
+     * key = 2, value = C
+     * key = 3, value = Python
+     */
+      for (Map.Entry<Integer, String> entry : map.entrySet()) {
+          System.out.println(
+                  "key = ".concat(entry.getKey() + "").concat(", value = ").concat(entry.getValue()));
+      }
+  }
+}
