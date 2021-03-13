87e618f17ed4c5f1b7432a23ceb3b48ffa83b982
shellhub <shellhub.me@gmail.com>
2021-03-13 10:43:07 +0800

    shuffle-the-array

---
+++ src/test/java/com/examplehub/leetcode/easy/ShuffleTheArrayTest.java
@@ -0,0 +1,16 @@
+package com.examplehub.leetcode.easy;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShuffleTheArrayTest {
+    @Test
+    void testSolution1() {
+        int[] nums = {2, 5, 1, 3, 4, 7};
+        ShuffleTheArray.solution1(nums, 3);
+        assertEquals(Arrays.toString(nums), Arrays.toString(new int[]{2, 3, 5, 4, 1, 7}));
+    }
+}
