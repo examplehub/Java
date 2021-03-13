e7eb97cab04e3ff1579e77bc14ecc9bd1a0625d6
shellhub <shellhub.me@gmail.com>
2021-03-13 11:03:42 +0800

    number-of-good-pairs

---
+++ src/main/java/com/examplehub/leetcode/easy/NumberOfGoodPairs.java
@@ -0,0 +1,18 @@
+package com.examplehub.leetcode.easy;
+
+/**
+ * https://leetcode.com/problems/number-of-good-pairs/
+ */
+public class NumberOfGoodPairs {
+    public static int solution1(int[] nums) {
+        int goodPairs = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    goodPairs++;
+                }
+            }
+        }
+        return goodPairs;
+    }
+}
