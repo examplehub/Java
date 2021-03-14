97629dc15adb95f5e78f14913c30a244565b0e8c
Du Yuanchao <shellhub.me@gmail.com>
2021-03-14 19:46:27 +0800

    leetcode-2021.3.14 (#72)
    * how-many-numbers-are-smaller-than-the-current-number
    
    * sum-of-all-odd-length-subarrays
    
    * Formatted with Google Java Formatter
    
    Co-authored-by: github-actions <${GITHUB_ACTOR}@users.noreply.github.com>

---
+++ src/main/java/com/examplehub/leetcode/easy/HowManyNumbersAreSmallerThanTheCurrentNumber.java
@@ -0,0 +1,20 @@
+package com.examplehub.leetcode.easy;
+
+/** https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/ */
+public class HowManyNumbersAreSmallerThanTheCurrentNumber {
+
+  public static int[] solution1(int[] nums) {
+    int[] result = new int[nums.length];
+    for (int i = 0; i < nums.length; ++i) {
+      for (int j = 0; j < nums.length; ++j) {
+        if (i == j) {
+          continue;
+        }
+        if (nums[j] < nums[i]) {
+          result[i]++;
+        }
+      }
+    }
+    return result;
+  }
+}
