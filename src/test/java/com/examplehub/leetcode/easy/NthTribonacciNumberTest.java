e4b415bb26e621694025fc08028a730dcec19156
Du Yuanchao <shellhub.me@gmail.com>
2021-03-23 09:21:25 +0800

    Leetcode 2021.3.23 (#80)
    * n-th-tribonacci-number
    
    * n-th-tribonacci-number
    
    * Formatted with Google Java Formatter
    
    Co-authored-by: github-actions <${GITHUB_ACTOR}@users.noreply.github.com>

---
+++ src/test/java/com/examplehub/leetcode/easy/NthTribonacciNumberTest.java
@@ -0,0 +1,26 @@
+package com.examplehub.leetcode.easy;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class NthTribonacciNumberTest {
+  @Test
+  void testSolution1() {
+    assertEquals(NthTribonacciNumber.solution1(0), 0);
+    assertEquals(NthTribonacciNumber.solution1(1), 1);
+    assertEquals(NthTribonacciNumber.solution1(2), 1);
+    assertEquals(NthTribonacciNumber.solution1(4), 4);
+    assertEquals(NthTribonacciNumber.solution1(25), 1389537);
+  }
+
+  @Test
+  void testSolution2() {
+    assertEquals(NthTribonacciNumber.solution2(0), 0);
+    assertEquals(NthTribonacciNumber.solution2(1), 1);
+    assertEquals(NthTribonacciNumber.solution2(2), 1);
+    assertEquals(NthTribonacciNumber.solution2(4), 4);
+    assertEquals(NthTribonacciNumber.solution2(25), 1389537);
+    assertEquals(NthTribonacciNumber.solution2(35), 615693474);
+  }
+}
