bf5ae587caf1796de3a9eb59f3ebcb49b8f6f72a
shellhub <shellhub.me@gmail.com>
2021-03-23 14:43:33 +0800

    multiply class in a file

---
+++ src/main/java/com/examplehub/basics/MultiClassInAFile.java
@@ -0,0 +1,21 @@
+package com.examplehub.basics;
+
+public class MultiClassInAFile {
+    public static void main(String[] args) {
+        A a = new A();
+        B b = new B();
+        C c = new C();
+    }
+}
+
+class A {
+
+}
+
+class B {
+
+}
+
+class C {
+
+}
