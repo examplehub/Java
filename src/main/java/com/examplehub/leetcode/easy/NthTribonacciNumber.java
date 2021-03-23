5a109c7c3f5a3cb42cada502251816fc2eedc78e
shellhub <shellhub.me@gmail.com>
2021-03-23 09:00:07 +0800

    n-th-tribonacci-number

--- src/main/java/com/examplehub/leetcode/easy/NthTribonacciNumber.java
+++ src/main/java/com/examplehub/leetcode/easy/NthTribonacciNumber.java
@@ -15,4 +15,15 @@ public class NthTribonacciNumber {
             return solution1(n - 1) + solution1(n - 2) + solution1(n - 3);
         }
     }
+
+    public static int solution2(int n) {
+        int[] nums = new int[n + 3];
+        nums[0] = 0;
+        nums[1] = 1;
+        nums[2] = 1;
+        for (int i = 3; i <= n; ++i) {
+            nums[i] = nums[i - 1] + nums[i - 2] + nums[i - 3];
+        }
+        return nums[n];
+    }
 }
