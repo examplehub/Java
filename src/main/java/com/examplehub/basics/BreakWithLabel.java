62fecce413cc91daf867dd38a352432a2580b32e
Du Yuanchao <shellhub.me@gmail.com>
2021-03-23 15:56:35 +0800

    break with label (#84)
    * break with label
    
    * Formatted with Google Java Formatter
    
    Co-authored-by: github-actions <${GITHUB_ACTOR}@users.noreply.github.com>

---
+++ src/main/java/com/examplehub/basics/BreakWithLabel.java
@@ -0,0 +1,20 @@
+package com.examplehub.basics;
+
+public class BreakWithLabel {
+  public static void main(String[] args) {
+    label:
+    for (int i = 1; i <= 10; ++i) {
+      for (int j = 1; j <= 10; ++j) {
+        System.out.println("i = " + i + ", j = " + j);
+        if (j == 2) {
+          break label;
+        }
+      }
+    }
+    /*
+     * output:
+     * i = 1, j = 1
+     * i = 1, j = 2
+     */
+  }
+}
