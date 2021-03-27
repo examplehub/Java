b8b393d098ec2a62444d2b28f74daa4358ed1253
shellhub <shellhub.me@gmail.com>
2021-03-27 21:00:40 +0800

    convert-binary-number-in-a-linked-list-to-integer

---
+++ src/test/java/com/examplehub/leetcode/easy/ConvertBinaryNumberInALinkedListToIntegerTest.java
@@ -0,0 +1,25 @@
+package com.examplehub.leetcode.easy;
+
+import com.examplehub.leetcode.ListNode;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConvertBinaryNumberInALinkedListToIntegerTest {
+    @Test
+    void testSolution1() {
+        ListNode head = new ListNode(1);
+        ListNode node2 = new ListNode(0);
+        ListNode node3 = new ListNode(1);
+        head.next = node2;
+        node2.next = node3;
+
+        assertEquals(ConvertBinaryNumberInALinkedListToInteger.solution1(head), 5);
+
+        head = new ListNode(1);
+        assertEquals(ConvertBinaryNumberInALinkedListToInteger.solution1(head), 1);
+
+        head = new ListNode(0);
+        assertEquals(ConvertBinaryNumberInALinkedListToInteger.solution1(head), 0);
+    }
+}
