7018c36b6a6ccd628cd0bc9585283eb0c9c7ab04
Du Yuanchao <shellhub.me@gmail.com>
2021-03-28 12:29:24 +0800

    Middle of the linked list (#89)
    * middle-of-the-linked-list
    
    * Formatted with Google Java Formatter
    
    Co-authored-by: github-actions <${GITHUB_ACTOR}@users.noreply.github.com>

---
+++ src/main/java/com/examplehub/leetcode/easy/MiddleOfTheLinkedList.java
@@ -0,0 +1,31 @@
+package com.examplehub.leetcode.easy;
+
+import com.examplehub.leetcode.ListNode;
+
+/** https://leetcode.com/problems/middle-of-the-linked-list/ */
+public class MiddleOfTheLinkedList {
+
+  public static ListNode solution1(ListNode head) {
+    int len = 0;
+    ListNode p = head;
+    while (p != null) {
+      len++;
+      p = p.next;
+    }
+    p = head;
+    for (int i = 1; i <= len / 2; i++) {
+      p = p.next;
+    }
+    return p;
+  }
+
+  public static ListNode solution2(ListNode head) {
+    ListNode p1 = head;
+    ListNode p2 = head;
+    while (p1 != null && p1.next != null) {
+      p1 = p1.next.next;
+      p2 = p2.next;
+    }
+    return p2;
+  }
+}
