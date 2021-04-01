5a9ca5f417a52ffc531c7b15662d319f36ddca28
shellhub <shellhub.me@gmail.com>
2021-04-01 20:00:06 +0800

    reformat code

--- src/test/java/com/examplehub/leetcode/easy/ConvertBinaryNumberInALinkedListToIntegerTest.java
+++ src/test/java/com/examplehub/leetcode/easy/ConvertBinaryNumberInALinkedListToIntegerTest.java
@@ -3,23 +3,21 @@ package com.examplehub.leetcode.easy;
 import static org.junit.jupiter.api.Assertions.*;
 
 import com.examplehub.leetcode.ListNode;
+import com.examplehub.utils.NodeUtils;
 import org.junit.jupiter.api.Test;
 
 class ConvertBinaryNumberInALinkedListToIntegerTest {
   @Test
   void testSolution1() {
-    ListNode head = new ListNode(1);
-    ListNode node2 = new ListNode(0);
-    ListNode node3 = new ListNode(1);
-    head.next = node2;
-    node2.next = node3;
 
+    ListNode head = NodeUtils.makeList(1, 0, 1);
+    assertEquals("1->0->1", head.toString());
     assertEquals(ConvertBinaryNumberInALinkedListToInteger.solution1(head), 5);
 
-    head = new ListNode(1);
+    head = NodeUtils.makeList(1);
     assertEquals(ConvertBinaryNumberInALinkedListToInteger.solution1(head), 1);
 
-    head = new ListNode(0);
+    head = NodeUtils.makeList(0);
     assertEquals(ConvertBinaryNumberInALinkedListToInteger.solution1(head), 0);
   }
 }
