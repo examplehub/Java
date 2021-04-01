5d575e8e4ed8354e4e348f1f371f831e6cd4b4ff
shellhub <shellhub.me@gmail.com>
2021-04-01 19:55:16 +0800

    Remove Duplicates from Sorted List

---
+++ src/main/java/com/examplehub/leetcode/easy/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,21 @@
+package com.examplehub.leetcode.easy;
+
+import com.examplehub.leetcode.ListNode;
+
+/**
+ * https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+ */
+public class RemoveDuplicatesFromSortedList {
+
+    public static ListNode solution1(ListNode head) {
+        ListNode p = head;
+        while (p != null && p.next != null) {
+            if (p.val == p.next.val) {
+                p.next = p.next.next;
+            } else {
+                p = p.next;
+            }
+        }
+        return head;
+    }
+}
