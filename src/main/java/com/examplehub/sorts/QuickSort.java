03c0a54078871b926f440deb0bd92e5d3900feec
shellhub <shellhub.me@gmail.com>
2021-04-06 16:40:05 +0800

    refactor quicksort

--- src/main/java/com/examplehub/sorts/QuickSort.java
+++ src/main/java/com/examplehub/sorts/QuickSort.java
@@ -1,77 +1,74 @@
 package com.examplehub.sorts;
 
-import com.examplehub.utils.SortUtils;
-
 public class QuickSort implements Sort {
 
-  @Override
-  public void sort(int[] numbers) {
-    quickSort(numbers, 0, numbers.length - 1);
-  }
-
-  /**
-   * QuickSort algorithm implements.
-   *
-   * @param numbers the numbers to be sorted.
-   */
-  public void quickSort(int[] numbers, int left, int right) {
-    if (left >= right) {
-      return;
-    }
-    int pivot = numbers[right]; /* pick last element as pivot */
-    /* begin partition */
-    int i = left; /* left pointer */
-    int j = right - 1; /* right pointer */
-    while (i <= j) {
-      while (numbers[i] < pivot) {
-        i++; /* move i forward */
-      }
-      while (j >= 0 && numbers[j] >= pivot) {
-        j--; /* move j backward */
-      }
-      if (i < j) {
-        SortUtils.swap(numbers, i, j);
-        i++;
-        j--;
-      }
+    @Override
+    public void sort(int[] numbers) {
+        quickSort(numbers, 0, numbers.length - 1);
     }
 
-    SortUtils.swap(numbers, i, right);
+    private int partition(int[] number, int left, int right) {
+        int pivot = number[left];
+        while (left < right) {
+            while (left < right && number[right] >= pivot) {
+                right--;
+            }
+            number[left] = number[right];
 
-    quickSort(numbers, left, i - 1);
-    quickSort(numbers, i + 1, right);
-  }
-
-  @Override
-  public <T extends Comparable<T>> void sort(T[] array) {
-    quickSort(array, 0, array.length - 1);
-  }
-
-  public static <T extends Comparable<T>> void quickSort(T[] array, int left, int right) {
-    if (left >= right) {
-      return;
-    }
-    T pivot = array[right]; /* pick last element as pivot */
-    /* begin partition */
-    int i = left; /* left pointer */
-    int j = right - 1; /* right pointer */
-    while (i <= j) {
-      while (array[i].compareTo(pivot) < 0) {
-        i++; /* move i forward */
-      }
-      while (j >= 0 && array[j].compareTo(pivot) >= 0) {
-        j--; /* move j backward */
-      }
-      if (i < j) {
-        SortUtils.swap(array, i, j);
-        i++;
-        j--;
-      }
+            while (left < right && number[left] <= pivot) {
+                left++;
+            }
+            number[right] = number[left];
+        }
+        number[left] = pivot;
+        return left;
     }
 
-    SortUtils.swap(array, i, right);
+    /**
+     * QuickSort algorithm implements.
+     *
+     * @param numbers the numbers to be sorted.
+     */
+    public void quickSort(int[] numbers, int left, int right) {
+        if (left < right) {
+            int pivotIndex = partition(numbers, left, right);
+            quickSort(numbers, left, pivotIndex - 1);
+            quickSort(numbers, pivotIndex + 1, right);
+        }
+    }
 
-    quickSort(array, left, i - 1);
-    quickSort(array, i + 1, right);
-  }
+    /**
+     * Generic quickSort algorithm implements.
+     * @param array the array to be sorted.
+     * @param <T> the class of the objects in the array.
+     */
+    @Override
+    public <T extends Comparable<T>> void sort(T[] array) {
+        quickSort(array, 0, array.length - 1);
+    }
+
+    private static <T extends Comparable<T>> int partition(T[] array, int left, int right) {
+        T pivot = array[left];
+        while (left < right) {
+            while (left < right && array[right].compareTo(pivot) >= 0) {
+                right--;
+            }
+            array[left] = array[right];
+
+            while (left < right && array[left].compareTo(pivot) <= 0) {
+                left++;
+            }
+            array[right] = array[left];
+        }
+        array[left] = pivot;
+        return left;
+    }
+
+    public static <T extends Comparable<T>> void quickSort(T[] array, int left, int right) {
+        if (left < right) {
+            int pivotIndex = partition(array, left, right);
+            quickSort(array, left, pivotIndex - 1);
+            quickSort(array, pivotIndex + 1, right);
+        }
+    }
 }
