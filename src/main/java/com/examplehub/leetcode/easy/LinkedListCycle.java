3f185cd272868a373059766500a8ff58eedb30bb
Du Yuanchao <shellhub.me@gmail.com>
2021-04-06 09:07:32 +0800

    Linked list cycle (#93)
    * linked-list-cycle
    
    * Formatted with Google Java Formatter
    
    Co-authored-by: github-actions <${GITHUB_ACTOR}@users.noreply.github.com>

---
+++ src/main/java/com/examplehub/leetcode/easy/LinkedListCycle.java
@@ -0,0 +1,35 @@
+package com.examplehub.leetcode.easy;
+
+import com.examplehub.leetcode.ListNode;
+import java.util.HashSet;
+import java.util.Set;
+
+/** https://leetcode.com/problems/linked-list-cycle/ */
+public class LinkedListCycle {
+  public static boolean solution1(ListNode head) {
+    Set<ListNode> sets = new HashSet<>();
+    while (head != null) {
+      if (!sets.add(head)) {
+        return true;
+      }
+      head = head.next;
+    }
+    return false;
+  }
+
+  public static boolean solution2(ListNode head) {
+    if (head == null || head.next == null) {
+      return false;
+    }
+    ListNode slow = head;
+    ListNode fast = head.next;
+    while (slow != fast) {
+      if (fast == null || fast.next == null) {
+        return false;
+      }
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+    return true;
+  }
+}
