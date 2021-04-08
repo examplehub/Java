d9c8a07900a36e8ab27a9ea8f22fbae202a02902
Du Yuanchao <shellhub.me@gmail.com>
2021-04-08 16:53:33 +0800

    Next greater node in linked list (#98)
    * next-greater-node-in-linked-list
    
    * Formatted with Google Java Formatter
    
    Co-authored-by: github-actions <${GITHUB_ACTOR}@users.noreply.github.com>

---
+++ src/test/java/com/examplehub/leetcode/middle/NextGreaterNodeInLinkedListTest.java
@@ -0,0 +1,25 @@
+package com.examplehub.leetcode.middle;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.examplehub.leetcode.ListNode;
+import com.examplehub.utils.NodeUtils;
+import java.util.Arrays;
+import org.junit.jupiter.api.Test;
+
+class NextGreaterNodeInLinkedListTest {
+  @Test
+  void testSolution1() {
+    ListNode head = NodeUtils.makeList(2, 1, 5);
+    assertTrue(Arrays.equals(new int[] {5, 5, 0}, NextGreaterNodeInLinkedList.solution1(head)));
+
+    head = NodeUtils.makeList(2, 7, 4, 3, 5);
+    assertTrue(
+        Arrays.equals(new int[] {7, 0, 5, 5, 0}, NextGreaterNodeInLinkedList.solution1(head)));
+
+    head = NodeUtils.makeList(1, 7, 5, 1, 9, 2, 5, 1);
+    assertTrue(
+        Arrays.equals(
+            new int[] {7, 9, 9, 9, 0, 5, 0, 0}, NextGreaterNodeInLinkedList.solution1(head)));
+  }
+}
