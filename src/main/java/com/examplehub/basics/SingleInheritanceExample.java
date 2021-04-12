f6d223038b56652df267341f11090485c8433cf7
shellhub <shellhub.me@gmail.com>
2021-04-12 15:02:24 +0800

    single inheritance example

---
+++ src/main/java/com/examplehub/basics/SingleInheritanceExample.java
@@ -0,0 +1,20 @@
+package com.examplehub.basics;
+
+public class SingleInheritanceExample {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.bark(); /* barking... */
+        dog.eat(); /* eating... */
+    }
+}
+
+class Animal {
+    public void eat() {
+        System.out.println("eating...");
+    }
+}
+class Dog extends Animal{
+    public void bark() {
+        System.out.println("barking...");
+    }
+}
