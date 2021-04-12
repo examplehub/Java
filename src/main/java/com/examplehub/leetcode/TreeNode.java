ffbe7b84c3cbb4cf8c8ea83d08bf15c1d6f05e1a
shellhub <shellhub.me@gmail.com>
2021-04-12 10:10:33 +0800

    range-sum-of-bst

---
+++ src/main/java/com/examplehub/leetcode/TreeNode.java
@@ -0,0 +1,21 @@
+package com.examplehub.leetcode;
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode() {
+
+    }
+
+    public TreeNode(int val) {
+        this(val, null, null);
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
---
+++ src/test/java/com/examplehub/leetcode/easy/RangeSumOfBSTTest.java
@@ -0,0 +1,27 @@
+package com.examplehub.leetcode.easy;
+
+import com.examplehub.leetcode.TreeNode;
+import com.examplehub.utils.TreeUtils;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RangeSumOfBSTTest {
+    @Test
+    void testSolution1() {
+        TreeNode root = TreeUtils.createBST(10, 5, 15, 3, 7, 13, 18, 1, 6);
+        assertEquals(23, RangeSumOfBST.solution1(root, 6, 10));
+
+        root = TreeUtils.createBST(10, 5, 15, 3, 7, 18);
+        assertEquals(32, RangeSumOfBST.solution1(root, 7, 15));
+    }
+
+    @Test
+    void testSolution2() {
+        TreeNode root = TreeUtils.createBST(10, 5, 15, 3, 7, 13, 18, 1, 6);
+        assertEquals(23, RangeSumOfBST.solution2(root, 6, 10));
+
+        root = TreeUtils.createBST(10, 5, 15, 3, 7, 18);
+        assertEquals(32, RangeSumOfBST.solution2(root, 7, 15));
+    }
+}
