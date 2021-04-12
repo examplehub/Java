0c5606a5246b68689970ec4d44ff5f339ad3b22d
Du Yuanchao <shellhub.me@gmail.com>
2021-04-12 11:59:35 +0800

    Fixed ArrayList typo, Fixed merge sort bugs (#101)
    * update ArrayList
    
    * update merge sort
    
    * Formatted with Google Java Formatter
    
    Co-authored-by: github-actions <${GITHUB_ACTOR}@users.noreply.github.com>

--- src/main/java/com/examplehub/sorts/MergeSort.java
+++ src/main/java/com/examplehub/sorts/MergeSort.java
@@ -24,7 +24,7 @@ public class MergeSort implements Sort {
       int middle = (left + right) >> 1;
       mergeSort(numbers, left, middle);
       mergeSort(numbers, middle + 1, right);
-      merge(numbers, left, middle, right);
+      merge(numbers, left, right);
     }
   }
 
@@ -41,33 +41,38 @@ public class MergeSort implements Sort {
       int middle = (left + right) >> 1;
       mergeSort(array, left, middle);
       mergeSort(array, middle + 1, right);
-      merge(array, left, middle, right);
+      merge(array, left, right);
     }
   }
 
   /**
    * @param numbers the numbers to be merged.
    * @param left the left index of first sub array.
-   * @param middle the right index of second sub array.
    * @param right the right index of second sub array.
    */
-  public static void merge(int[] numbers, int left, int middle, int right) {
+  public static void merge(int[] numbers, int left, int right) {
+    int middle = (left + right) >> 1;
     int i = left;
     int j = middle + 1;
-    int k = left;
+    int k = 0;
+    int len = right - left + 1;
+    int[] temp = new int[len];
     while (i <= middle && j <= right) {
       if (numbers[i] <= numbers[j]) {
-        numbers[k++] = numbers[i++];
+        temp[k++] = numbers[i++];
       } else {
-        numbers[k++] = numbers[j++];
+        temp[k++] = numbers[j++];
       }
     }
     while (i <= middle) {
-      numbers[k++] = numbers[i++];
+      temp[k++] = numbers[i++];
     }
 
     while (j <= right) {
-      numbers[k++] = numbers[j++];
+      temp[k++] = numbers[j++];
+    }
+    for (int number : temp) {
+      numbers[left++] = number;
     }
   }
 
@@ -76,27 +81,33 @@ public class MergeSort implements Sort {
    *
    * @param array the array to be sorted.
    * @param left the left index of first sub array.
-   * @param middle the right index of second sub array.
    * @param right the right index of second sub array.
    * @param <T> the class of the objects in the array.
    */
-  public static <T extends Comparable<T>> void merge(T[] array, int left, int middle, int right) {
+  public static <T extends Comparable<T>> void merge(T[] array, int left, int right) {
+    int middle = (left + right) >> 1;
     int i = left;
     int j = middle + 1;
-    int k = left;
+    int k = 0;
+    @SuppressWarnings("unchecked")
+    T[] tempArray = (T[]) new Comparable[right - left + 1];
     while (i <= middle && j <= right) {
       if (array[i].compareTo(array[j]) <= 0) {
-        array[k++] = array[i++];
+        tempArray[k++] = array[i++];
       } else {
-        array[k++] = array[j++];
+        tempArray[k++] = array[j++];
       }
     }
     while (i <= middle) {
-      array[k++] = array[i++];
+      tempArray[k++] = array[i++];
     }
 
     while (j <= right) {
-      array[k++] = array[j++];
+      tempArray[k++] = array[j++];
+    }
+
+    for (T temp : tempArray) {
+      array[left++] = temp;
     }
   }
 }
