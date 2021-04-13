342f8260781a75246e8d07fba1e7fbf912c0eb7a
shellhub <shellhub.me@gmail.com>
2021-04-13 08:38:50 +0800

    function overriding

---
+++ src/main/java/com/examplehub/basics/FunctionOverriding.java
@@ -0,0 +1,20 @@
+package com.examplehub.basics;
+
+public class FunctionOverriding {
+    public static void main(String[] args) {
+        Vehicle bike = new Bike();
+        bike.run(); /* Bike is running. */
+    }
+}
+class Vehicle {
+    public void run() {
+        System.out.println("Vehicle is running.");
+    }
+}
+
+class Bike extends Vehicle {
+    @Override
+    public void run() {
+        System.out.println("Bike is running.");
+    }
+}
