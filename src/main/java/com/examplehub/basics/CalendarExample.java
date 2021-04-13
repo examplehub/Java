39b567270db26dcb02e804c1c88ea2789c0238ee
shellhub <shellhub.me@gmail.com>
2021-04-13 19:10:47 +0800

    date example

---
+++ src/main/java/com/examplehub/basics/CalendarExample.java
@@ -0,0 +1,10 @@
+package com.examplehub.basics;
+
+import java.util.Calendar;
+
+public class CalendarExample {
+    public static void main(String[] args) {
+        Calendar calendar = Calendar.getInstance();
+        System.out.println(calendar);
+    }
+}
