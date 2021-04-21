db727eb103093f084da118325e0dc2f5193b4212
shellhub <shellhub.me@gmail.com>
2021-04-21 09:14:12 +0800

    middle index calculate

---
+++ src/main/java/com/examplehub/maths/MiddleIndexCalculate.java
@@ -0,0 +1,15 @@
+package com.examplehub.maths;
+
+public class MiddleIndexCalculate {
+
+    /**
+     * Return middle index from left index to right index.
+     *
+     * @param left  the left index of array.
+     * @param right the right index of array.
+     * @return middle index.
+     */
+    public static int middle(int left, int right) {
+        return (left + right) >>> 1;
+    }
+}
