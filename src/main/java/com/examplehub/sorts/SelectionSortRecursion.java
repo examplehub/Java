10140cb233e78bf6ca6bf2d35e8f581ad00400ac
Du Yuanchao <shellhub.me@gmail.com>
2021-05-06 14:41:33 +0800

    Selection sort recursion (#118)
    * selection sort recursion
    
    * Formatted with Google Java Formatter
    
    Co-authored-by: github-actions <${GITHUB_ACTOR}@users.noreply.github.com>

---
+++ src/main/java/com/examplehub/sorts/SelectionSortRecursion.java
@@ -0,0 +1,61 @@
+package com.examplehub.sorts;
+
+public class SelectionSortRecursion implements Sort {
+
+  @Override
+  public void sort(int[] numbers) {
+    selectionSort(numbers, numbers.length);
+  }
+
+  /**
+   * Selection sort algorithm implements.
+   *
+   * @param numbers the numbers to be sorted.
+   * @param len the len of sub array.
+   */
+  private void selectionSort(int[] numbers, int len) {
+    if (len > 1) {
+      int maxIndex = len - 1;
+      for (int j = len - 2; j >= 0; j--) {
+        if (numbers[j] > numbers[maxIndex]) {
+          maxIndex = j;
+        }
+      }
+      if (maxIndex != len - 1) {
+        int temp = numbers[maxIndex];
+        numbers[maxIndex] = numbers[len - 1];
+        numbers[len - 1] = temp;
+      }
+      selectionSort(numbers, len - 1);
+    }
+  }
+
+  @Override
+  public <T extends Comparable<T>> void sort(T[] array) {
+    selectionSort(array, array.length);
+  }
+
+  /**
+   * Generic SelectionSort algorithm implements.
+   *
+   * @param numbers the array to be sorted.
+   * @param len the length of sub array.
+   * @param <T> the class of the objects in the array.
+   */
+  private <T extends Comparable<T>> void selectionSort(T[] numbers, int len) {
+    if (len > 1) {
+      int maxIndex = len - 1;
+      for (int j = len - 2; j >= 0; j--) {
+        if (numbers[j].compareTo(numbers[maxIndex]) > 0) {
+          maxIndex = j;
+        }
+      }
+      if (maxIndex != len - 1) {
+        T temp = numbers[maxIndex];
+        numbers[maxIndex] = numbers[len - 1];
+        numbers[len - 1] = temp;
+      }
+      selectionSort(numbers, len - 1);
+    }
+  }
+}
---
+++ src/test/java/com/examplehub/sorts/SelectionSortRecursionTest.java
@@ -0,0 +1,33 @@
+package com.examplehub.sorts;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.examplehub.utils.RandomUtils;
+import com.examplehub.utils.SortUtils;
+import java.util.Arrays;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SelectionSortRecursionTest {
+  private Sort sort;
+
+  @BeforeEach
+  public void before() {
+    sort = new SelectionSortRecursion();
+  }
+
+  @Test
+  void testSort() {
+    int[] ints = RandomUtils.randomInts(-50, 50, 100);
+    sort.sort(ints);
+    assertTrue(SortUtils.isSorted(ints));
+  }
+
+  @Test
+  void sortInteger() {
+    Integer[] integers =
+        Arrays.stream(RandomUtils.randomInts(-50, 50, 100)).boxed().toArray(Integer[]::new);
+    sort.sort(integers);
+    assertTrue(SortUtils.isSorted(integers));
+  }
+}
