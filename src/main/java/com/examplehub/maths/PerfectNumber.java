19ad0316aa2072e67957c8517d0eb5e6639191f6
shellhub <shellhub.me@gmail.com>
2021-05-21 19:21:40 +0800

    optimization for perfect number

--- src/main/java/com/examplehub/maths/PerfectNumber.java
+++ src/main/java/com/examplehub/maths/PerfectNumber.java
@@ -14,7 +14,7 @@ public class PerfectNumber {
       return false; /* all negative is not perfect number */
     }
     int sum = 0;
-    for (int i = 1; i < number / 2 + 1; ++i) {
+    for (int i = 1; i <= number / 2; ++i) {
 
       if (number % i == 0) {
         sum += i;
