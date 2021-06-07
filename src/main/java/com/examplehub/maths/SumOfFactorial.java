5c9eb49f95f843dbe0ca6e28f7ceea2a2b4a94bf
shellhub <shellhub.me@gmail.com>
2021-06-07 21:30:26 +0800

    sum of factorial

--- src/main/java/com/examplehub/maths/SumOfFactorial.java
+++ src/main/java/com/examplehub/maths/SumOfFactorial.java
@@ -2,15 +2,28 @@ package com.examplehub.maths;
 
 /** Calculate 1! + 2! + 3! + ... n! */
 public class SumOfFactorial {
-  public static int sum(int n) {
-    return sum(1, n);
-  }
 
   public static int sum(int i, int n) {
     if (i == n) {
       return n;
     } else {
       return i + i * sum(i + 1, n);
+    }
+  }
+
+  public static int sum(int n) {
+    if (n == 1) {
+      return 1; /* 1! */
+    } else if (n == 2) {
+      return 1 + 2; /* 1! + 2! */
+    } else {
+      /**
+       * sum(n)=1! + 2! + 3! + ... + (n-1)! + n!
+       *       = sum(n-1) + n!
+       *       = sum(n-1) + (n-1)! * n
+       *       = sum(n-1) + (sum(n-1) - sum(n-2))*n
+       */
+      return sum(n - 1) + (sum(n - 1) - sum(n - 2)) * n;
     }
   }
 }
