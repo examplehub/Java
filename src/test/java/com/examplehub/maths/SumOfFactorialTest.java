27a03f6366705ba4a0ffe9aed52971627c9d1d0a
Du Yuanchao <shellhub.me@gmail.com>
2021-06-08 14:07:16 +0800

    sum of factorial (#126)
    * sum of factorial
    
    * Formatted with Google Java Formatter
    
    Co-authored-by: github-actions <${GITHUB_ACTOR}@users.noreply.github.com>

--- src/test/java/com/examplehub/maths/SumOfFactorialTest.java
+++ src/test/java/com/examplehub/maths/SumOfFactorialTest.java
@@ -7,6 +7,12 @@ import org.junit.jupiter.api.Test;
 class SumOfFactorialTest {
   @Test
   void testSum() {
+    assertEquals(1, SumOfFactorial.sum(1, 1));
+    assertEquals(3, SumOfFactorial.sum(1, 2));
+    assertEquals(9, SumOfFactorial.sum(1, 3));
+    assertEquals(33, SumOfFactorial.sum(1, 4));
+    assertEquals(153, SumOfFactorial.sum(1, 5));
+
     assertEquals(1, SumOfFactorial.sum(1));
     assertEquals(3, SumOfFactorial.sum(2));
     assertEquals(9, SumOfFactorial.sum(3));
