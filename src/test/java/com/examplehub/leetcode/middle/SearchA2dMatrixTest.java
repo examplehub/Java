efd14ca334ead0aa48d1a76e5f00be2f938596c7
Du Yuanchao <shellhub.me@gmail.com>
2021-07-02 09:12:06 +0800

    search a 2d matrix (#128)
    * search a 2d matrix
    
    * Formatted with Google Java Formatter
    
    Co-authored-by: github-actions <${GITHUB_ACTOR}@users.noreply.github.com>

---
+++ src/test/java/com/examplehub/leetcode/middle/SearchA2dMatrixTest.java
@@ -0,0 +1,29 @@
+package com.examplehub.leetcode.middle;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SearchA2dMatrixTest {
+  @Test
+  void testSolution1() {
+    int[][] matrix = {
+      {1, 3, 5, 7},
+      {10, 11, 16, 20},
+      {23, 30, 34, 60}
+    };
+    assertTrue(SearchA2dMatrix.solution1(matrix, 3));
+    assertFalse(SearchA2dMatrix.solution1(matrix, 13));
+  }
+
+  @Test
+  void testSolution2() {
+    int[][] matrix = {
+      {1, 3, 5, 7},
+      {10, 11, 16, 20},
+      {23, 30, 34, 60}
+    };
+    assertTrue(SearchA2dMatrix.solution2(matrix, 3));
+    assertFalse(SearchA2dMatrix.solution2(matrix, 13));
+  }
+}
