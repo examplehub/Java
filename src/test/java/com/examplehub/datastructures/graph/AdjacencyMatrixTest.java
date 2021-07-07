f7ec0defa5cbe06d3894bf1635aa941bcb778e07
Du Yuanchao <shellhub.me@gmail.com>
2021-07-07 20:01:54 +0800

    find all paths of graph (#132)
    * find all path of graph
    
    * Formatted with Google Java Formatter
    
    Co-authored-by: github-actions <${GITHUB_ACTOR}@users.noreply.github.com>

--- src/test/java/com/examplehub/datastructures/graph/AdjacencyMatrixTest.java
+++ src/test/java/com/examplehub/datastructures/graph/AdjacencyMatrixTest.java
@@ -48,4 +48,22 @@ class AdjacencyMatrixTest {
     assertEquals(4, adjacencyMatrix.getVertexDegree("D"));
     assertEquals(2, adjacencyMatrix.getVertexDegree("E"));
   }
+
+  @Test
+  void testFindAllPath() {
+    String[] vertex = {"A", "B", "C", "D", "E"};
+    AdjacencyMatrix<String> adjacencyMatrix = new AdjacencyMatrix<>(vertex);
+    adjacencyMatrix.addEdge(0, 1); /* A - B */
+    adjacencyMatrix.addEdge(0, 2); /* A - C */
+    adjacencyMatrix.addEdge(0, 3); /* A - D */
+    adjacencyMatrix.addEdge(1, 3); /* B - D */
+    adjacencyMatrix.addEdge(2, 3); /* C - D */
+    adjacencyMatrix.addEdge(1, 4); /* B - E */
+    adjacencyMatrix.addEdge(3, 4); /* D - E */
+    assertEquals(
+        "A->B->D->E, A->B->E, A->C->D->B->E, A->C->D->E, A->D->B->E, A->D->E",
+        String.join(", ", adjacencyMatrix.findAllPath("A", "E")));
+
+    assertNull(adjacencyMatrix.findAllPath("A", "F"));
+  }
 }
