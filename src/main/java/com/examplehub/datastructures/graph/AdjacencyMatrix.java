1a752e05922d319f33f10f09fc291b66059f6af1
shellhub <shellhub.me@gmail.com>
2021-07-07 19:52:29 +0800

    find all path of graph

--- src/main/java/com/examplehub/datastructures/graph/AdjacencyMatrix.java
+++ src/main/java/com/examplehub/datastructures/graph/AdjacencyMatrix.java
@@ -1,5 +1,8 @@
 package com.examplehub.datastructures.graph;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
 import java.util.StringJoiner;
 
 public class AdjacencyMatrix<E> {
@@ -8,7 +11,6 @@ public class AdjacencyMatrix<E> {
   private final boolean[] visited;
   int numOfVertex;
   private final StringJoiner dfsPath = new StringJoiner("->");
-
   public AdjacencyMatrix(E[] vertex) {
     this.vertex = vertex;
     this.numOfVertex = vertex.length;
@@ -56,5 +58,50 @@ public class AdjacencyMatrix<E> {
       deepFirstSearch(0);
     }
     return dfsPath.toString();
+  }
+
+  List<String> findAllPath(E startVertex, E endVertex) {
+    int start = -1;
+    int end = -1;
+    for (int i = 0; i < numOfVertex; ++i) {
+      if (startVertex == vertex[i]) {
+        start = i;
+      }
+      if (endVertex == vertex[i]) {
+        end = i;
+      }
+    }
+    return (start == -1 || end == -1) ? null : findAllPath(start, end);
+  }
+
+  private List<String> findAllPath(int start, int end) {
+    List<String> allPath = new ArrayList<>();
+    Stack<E> stack = new Stack<>();
+    boolean[] visited = new boolean[vertex.length];
+    doFindAllPath(allPath, stack, visited, start, end);
+    return allPath;
+  }
+
+  void doFindAllPath(List<String> allPath, Stack<E> stack, boolean[] visited, int start, int end) {
+    stack.push(vertex[start]);
+    visited[start] = true;
+    if (start == end) {
+      StringJoiner joiner = new StringJoiner("->");
+      for (E item : stack) {
+        joiner.add(item.toString());
+      }
+      allPath.add(joiner.toString());
+      stack.pop();
+      visited[start] = false;
+      return;
+    }
+
+    for (int i = 0; i < numOfVertex; ++i) {
+      if (adj[start][i] == 1 && !visited[i]) {
+        doFindAllPath(allPath, stack, visited, i, end);
+      }
+    }
+    stack.pop();
+    visited[start] = false;
   }
 }
--- src/test/java/com/examplehub/datastructures/graph/AdjacencyMatrixTest.java
+++ src/test/java/com/examplehub/datastructures/graph/AdjacencyMatrixTest.java
@@ -3,49 +3,72 @@ package com.examplehub.datastructures.graph;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.util.CollectionUtils;
+
+import java.util.Collections;
 
 class AdjacencyMatrixTest {
-  @Test
-  void test() {
-    String[] vertex = {"A", "B", "C", "D", "E"};
-    int[][] adj = {
-      {0, 1, 1, 1, 0},
-      {1, 0, 0, 1, 1},
-      {1, 0, 0, 1, 0},
-      {1, 1, 1, 1, 1},
-      {0, 1, 0, 1, 0}
-    };
-    AdjacencyMatrix<String> adjacencyMatrix = new AdjacencyMatrix<>(vertex, adj);
-    assertEquals("A->B->D->C->E", adjacencyMatrix.dfsPath());
+    @Test
+    void test() {
+        String[] vertex = {"A", "B", "C", "D", "E"};
+        int[][] adj = {
+                {0, 1, 1, 1, 0},
+                {1, 0, 0, 1, 1},
+                {1, 0, 0, 1, 0},
+                {1, 1, 1, 1, 1},
+                {0, 1, 0, 1, 0}
+        };
+        AdjacencyMatrix<String> adjacencyMatrix = new AdjacencyMatrix<>(vertex, adj);
+        assertEquals("A->B->D->C->E", adjacencyMatrix.dfsPath());
 
-    adjacencyMatrix = new AdjacencyMatrix<>(vertex);
-    adjacencyMatrix.addEdge(0, 1); /* A - B */
-    adjacencyMatrix.addEdge(0, 2); /* A - C */
-    adjacencyMatrix.addEdge(0, 3); /* A - D */
-    adjacencyMatrix.addEdge(1, 3); /* B - D */
-    adjacencyMatrix.addEdge(2, 3); /* C - D */
-    adjacencyMatrix.addEdge(1, 4); /* B - E */
-    adjacencyMatrix.addEdge(3, 4); /* D - E */
-    assertEquals("A->B->D->C->E", adjacencyMatrix.dfsPath());
-  }
+        adjacencyMatrix = new AdjacencyMatrix<>(vertex);
+        adjacencyMatrix.addEdge(0, 1); /* A - B */
+        adjacencyMatrix.addEdge(0, 2); /* A - C */
+        adjacencyMatrix.addEdge(0, 3); /* A - D */
+        adjacencyMatrix.addEdge(1, 3); /* B - D */
+        adjacencyMatrix.addEdge(2, 3); /* C - D */
+        adjacencyMatrix.addEdge(1, 4); /* B - E */
+        adjacencyMatrix.addEdge(3, 4); /* D - E */
+        assertEquals("A->B->D->C->E", adjacencyMatrix.dfsPath());
+    }
 
-  @Test
-  void testGetVertexDegree() {
-    String[] vertex = {"A", "B", "C", "D", "E"};
-    AdjacencyMatrix<String> adjacencyMatrix = new AdjacencyMatrix<>(vertex);
-    adjacencyMatrix.addEdge(0, 1); /* A - B */
-    adjacencyMatrix.addEdge(0, 2); /* A - C */
-    adjacencyMatrix.addEdge(0, 3); /* A - D */
-    adjacencyMatrix.addEdge(1, 3); /* B - D */
-    adjacencyMatrix.addEdge(2, 3); /* C - D */
-    adjacencyMatrix.addEdge(1, 4); /* B - E */
-    adjacencyMatrix.addEdge(3, 4); /* D - E */
-    assertEquals("A->B->D->C->E", adjacencyMatrix.dfsPath());
+    @Test
+    void testGetVertexDegree() {
+        String[] vertex = {"A", "B", "C", "D", "E"};
+        AdjacencyMatrix<String> adjacencyMatrix = new AdjacencyMatrix<>(vertex);
+        adjacencyMatrix.addEdge(0, 1); /* A - B */
+        adjacencyMatrix.addEdge(0, 2); /* A - C */
+        adjacencyMatrix.addEdge(0, 3); /* A - D */
+        adjacencyMatrix.addEdge(1, 3); /* B - D */
+        adjacencyMatrix.addEdge(2, 3); /* C - D */
+        adjacencyMatrix.addEdge(1, 4); /* B - E */
+        adjacencyMatrix.addEdge(3, 4); /* D - E */
+        assertEquals("A->B->D->C->E", adjacencyMatrix.dfsPath());
 
-    assertEquals(3, adjacencyMatrix.getVertexDegree("A"));
-    assertEquals(3, adjacencyMatrix.getVertexDegree("B"));
-    assertEquals(2, adjacencyMatrix.getVertexDegree("C"));
-    assertEquals(4, adjacencyMatrix.getVertexDegree("D"));
-    assertEquals(2, adjacencyMatrix.getVertexDegree("E"));
-  }
+        assertEquals(3, adjacencyMatrix.getVertexDegree("A"));
+        assertEquals(3, adjacencyMatrix.getVertexDegree("B"));
+        assertEquals(2, adjacencyMatrix.getVertexDegree("C"));
+        assertEquals(4, adjacencyMatrix.getVertexDegree("D"));
+        assertEquals(2, adjacencyMatrix.getVertexDegree("E"));
+    }
+
+    @Test
+    void testFindAllPath() {
+        String[] vertex = {"A", "B", "C", "D", "E"};
+        AdjacencyMatrix<String> adjacencyMatrix = new AdjacencyMatrix<>(vertex);
+        adjacencyMatrix.addEdge(0, 1); /* A - B */
+        adjacencyMatrix.addEdge(0, 2); /* A - C */
+        adjacencyMatrix.addEdge(0, 3); /* A - D */
+        adjacencyMatrix.addEdge(1, 3); /* B - D */
+        adjacencyMatrix.addEdge(2, 3); /* C - D */
+        adjacencyMatrix.addEdge(1, 4); /* B - E */
+        adjacencyMatrix.addEdge(3, 4); /* D - E */
+        assertEquals(
+              "A->B->D->E, A->B->E, A->C->D->B->E, A->C->D->E, A->D->B->E, A->D->E",
+              String.join(", ", adjacencyMatrix.findAllPath("A", "E"))
+      );
+
+        assertNull(adjacencyMatrix.findAllPath("A", "F"));
+
+    }
 }
