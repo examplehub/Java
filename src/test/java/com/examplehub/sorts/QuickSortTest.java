200b7f53b3835fea8e14a502005f8d57bb4758b8
shellhub <shellhub.me@gmail.com>
2021-08-03 19:40:53 +0800

    add test

--- src/test/java/com/examplehub/sorts/QuickSortTest.java
+++ src/test/java/com/examplehub/sorts/QuickSortTest.java
@@ -22,6 +22,10 @@ class QuickSortTest {
     int[] ints = RandomUtils.randomInts(-50, 50, 100);
     sort.sort(ints);
     assertTrue(SortUtils.isSorted(ints));
+
+    ints = new int[]{0, 1, 3, 5, 7, 9, 2, 4, 6, 8, 10};
+    sort.sort(ints);
+    assertTrue(SortUtils.isSorted(ints));
   }
 
   @Test
