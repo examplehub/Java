a1a1d8d573c2d12052a12c2d13b0571e27ce417a
Du Yuanchao <shellhub.me@gmail.com>
2021-09-11 07:50:00 +0800

    thread tutorial (#180)
    * thread tutorial
    
    * remove log.txt

---
+++ src/test/java/com/examplehub/basics/thread/InterruptedThreadTest.java
@@ -0,0 +1,28 @@
+package com.examplehub.basics.thread;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+class InterruptedThreadTest {
+  @Test
+  @Disabled
+  void test() throws InterruptedException {
+    //TODO
+    class MyThread extends Thread {
+      @Override
+      public void run() {
+        while (!isInterrupted()) {
+          System.out.println(Thread.currentThread().getName() + " running");
+        }
+        System.out.println(Thread.currentThread().getName() + " stopped");
+      }
+    }
+
+    Thread thread = new MyThread();
+    thread.start();
+    Thread.sleep(1);
+    thread.interrupt();
+    thread.join();
+    System.out.println(Thread.currentThread().getName() + " stopped");
+  }
+}
