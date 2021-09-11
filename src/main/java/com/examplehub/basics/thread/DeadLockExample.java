28904cf799fbb5177faab19c05b410443d9837b7
shellhub <shellhub.me@gmail.com>
2021-09-11 09:43:09 +0800

    thread lock

--- src/main/java/com/examplehub/basics/thread/DeadLockExample.java
+++ src/main/java/com/examplehub/basics/thread/DeadLockExample.java
@@ -1,48 +1,42 @@
 package com.examplehub.basics.thread;
 
 public class DeadLockExample {
-  public Object resourceA = new Object();
-  public Object resourceB = new Object();
+  public final Object resourceA = new Object();
+  public final Object resourceB = new Object();
 
   public static void main(String[] args) {
     DeadLockExample deadLockExample = new DeadLockExample();
     Runnable runnableA =
-        new Runnable() {
-          @Override
-          public void run() {
-            synchronized (deadLockExample.resourceA) {
-              System.out.println(Thread.currentThread().getName() + "get resourceA");
-              try {
-                Thread.sleep(1000);
-              } catch (InterruptedException e) {
-                e.printStackTrace();
-              }
-              System.out.println(Thread.currentThread().getName() + " is trying to get resourceB");
-              synchronized (deadLockExample.resourceB) {
-                System.out.println(Thread.currentThread().getName() + "get resourceB");
-              }
-            }
-          }
-        };
-
-    Runnable runnableB =
-        new Runnable() {
-          @Override
-          public void run() {
-            synchronized (deadLockExample.resourceB) {
-              System.out.println(Thread.currentThread().getName() + "get resourceB");
-              try {
-                Thread.sleep(1000);
-              } catch (InterruptedException e) {
-                e.printStackTrace();
-              }
-              System.out.println(Thread.currentThread().getName() + " is trying to get resourceA");
+            () -> {
               synchronized (deadLockExample.resourceA) {
                 System.out.println(Thread.currentThread().getName() + "get resourceA");
+                try {
+                  Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                  e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread().getName() + " is trying to get resourceB");
+                synchronized (deadLockExample.resourceB) {
+                  System.out.println(Thread.currentThread().getName() + "get resourceB");
+                }
               }
-            }
-          }
-        };
+            };
+
+    Runnable runnableB =
+            () -> {
+              synchronized (deadLockExample.resourceB) {
+                System.out.println(Thread.currentThread().getName() + "get resourceB");
+                try {
+                  Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                  e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread().getName() + " is trying to get resourceA");
+                synchronized (deadLockExample.resourceA) {
+                  System.out.println(Thread.currentThread().getName() + "get resourceA");
+                }
+              }
+            };
 
     new Thread(runnableA).start();
     new Thread(runnableB).start();
---
+++ src/test/java/com/examplehub/basics/thread/WaitNotifyExampleTest.java
@@ -0,0 +1,63 @@
+package com.examplehub.basics.thread;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class WaitNotifyExampleTest {
+  static class TaskQueue {
+    Queue<String> queue = new LinkedList<>();
+
+    public synchronized void addTask(String task) {
+      queue.add(task);
+      this.notifyAll();
+    }
+
+    public synchronized String getTask() throws InterruptedException {
+      while (queue.isEmpty()) {
+        this.wait();
+      }
+      return queue.remove();
+    }
+  }
+  @Test
+  void test() throws InterruptedException {
+    var tasks = new TaskQueue();
+    var threads = new ArrayList<Thread>();
+    for (int i = 0; i < 5; i++) {
+      Thread thread = new Thread(()->{
+        while (true) {
+          try {
+            String task = tasks.getTask();
+            System.out.println("execute: " + task);
+          } catch (InterruptedException e) {
+            System.out.println("thread " + Thread.currentThread().getName() + " interrupted" );
+            return;
+          }
+        }
+      });
+      thread.start();
+      threads.add(thread);
+    }
+    var addThread = new Thread(()->{
+      for (int i = 0; i < 10; ++i) {
+        String task = "task - " + Math.random();
+        System.out.println("added: " + task);
+        tasks.addTask(task);
+        try {
+          Thread.sleep(100);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+      }
+    });
+    addThread.start();
+    addThread.join();
+    Thread.sleep(100);
+    for (var thread : threads) {
+      thread.interrupt();
+    }
+  }
+}
