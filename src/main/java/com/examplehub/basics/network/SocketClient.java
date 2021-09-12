19363ecabbd9127a59f2efb410407a95be0fbfed
Du Yuanchao <shellhub.me@gmail.com>
2021-09-12 17:03:13 +0800

    update socket (#183)

---
+++ src/main/java/com/examplehub/basics/network/SocketClient.java
@@ -0,0 +1,37 @@
+package com.examplehub.basics.network;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+public class SocketClient {
+  public static void main(String[] args) throws IOException {
+    Socket clientSocket = new Socket("127.0.0.1", 6666);
+    try (InputStream inputStream = clientSocket.getInputStream()) {
+      try (OutputStream outputStream = clientSocket.getOutputStream()) {
+        handle(inputStream, outputStream);
+      }
+    }
+    clientSocket.close();
+    System.out.println("disconnected.");
+  }
+  private static void handle(InputStream inputStream, OutputStream outputStream) throws IOException {
+    var writer = new BufferedWriter(new OutputStreamWriter(outputStream, StandardCharsets.UTF_8));
+    var reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("[server] " + reader.readLine());
+    for (;;) {
+      System.out.print(">>> "); // 打印提示
+      String s = scanner.nextLine(); // 读取一行输入
+      writer.write(s + System.lineSeparator());
+//      writer.newLine();
+      writer.flush();
+      String resp = reader.readLine();
+      System.out.println("<<< " + resp);
+      if (resp.equals("bye")) {
+        break;
+      }
+    }
+  }
+}
