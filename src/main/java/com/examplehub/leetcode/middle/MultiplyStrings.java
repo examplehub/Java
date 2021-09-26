4272fe70540e9849b13a39fa13ec2d0429feecd9
Du Yuanchao <shellhub.me@gmail.com>
2021-09-26 22:51:54 +0800

    multiply strings (#185)

---
+++ src/main/java/com/examplehub/leetcode/middle/MultiplyStrings.java
@@ -0,0 +1,22 @@
+package com.examplehub.leetcode.middle;
+
+import java.math.BigInteger;
+
+/**
+ * https://leetcode.com/problems/multiply-strings/
+ */
+public class MultiplyStrings {
+  public static String solution1(String num1, String num2) {
+    return String.valueOf(Integer.parseInt(num1) * Integer.parseInt(num2));
+  }
+
+  public static String solution2(String num1, String num2) {
+    BigInteger multiply = new BigInteger(num1).multiply(new BigInteger(num2));
+    return multiply.toString(10);
+  }
+
+  public static String solution3(String num1, String num2) {
+    //TODO
+    return "";
+  }
+}
---
+++ src/test/java/com/examplehub/leetcode/middle/MultiplyStringsTest.java
@@ -0,0 +1,16 @@
+package com.examplehub.leetcode.middle;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MultiplyStringsTest {
+  @Test
+  void testSolution1() {
+    assertEquals("6", MultiplyStrings.solution1("2", "3"));
+  }
+  @Test
+  void testSolution2() {
+    assertEquals("6", MultiplyStrings.solution2("2", "3"));
+  }
+}
