7d402d22cd9a7d99399c33fde846b1dd4fe5ccfd
Du Yuanchao <duyuanchao.me@gmail.com>
2021-11-10 09:23:17 +0800

    update tcp

--- src/main/java/com/examplehub/basics/network/SocketClient.java
+++ src/main/java/com/examplehub/basics/network/SocketClient.java
@@ -8,29 +8,26 @@ import java.util.Scanner;
 public class SocketClient {
   public static void main(String[] args) throws IOException {
     Socket clientSocket = new Socket("127.0.0.1", 6666);
-    try (InputStream inputStream = clientSocket.getInputStream()) {
-      try (OutputStream outputStream = clientSocket.getOutputStream()) {
-        handle(inputStream, outputStream);
-      }
+    try (InputStream inputStream = clientSocket.getInputStream(); OutputStream outputStream = clientSocket.getOutputStream()) {
+      handle(inputStream, outputStream);
     }
     clientSocket.close();
     System.out.println("disconnected.");
   }
 
   private static void handle(InputStream inputStream, OutputStream outputStream)
-      throws IOException {
+          throws IOException {
     var writer = new BufferedWriter(new OutputStreamWriter(outputStream, StandardCharsets.UTF_8));
     var reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
     Scanner scanner = new Scanner(System.in);
     System.out.println("[server] " + reader.readLine());
     for (; ; ) {
-      System.out.print(">>> "); // 打印提示
-      String s = scanner.nextLine(); // 读取一行输入
+      System.out.print(">>> ");
+      String s = scanner.nextLine();
       writer.write(s + System.lineSeparator());
-      //      writer.newLine();
       writer.flush();
       String resp = reader.readLine();
-      System.out.println("<<< " + resp);
+      System.out.println("[server]: " + resp);
       if (resp.equals("bye")) {
         break;
       }
